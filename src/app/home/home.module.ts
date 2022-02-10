b5f569d95a10cc4197b5c2ba5e213b6b6a6d2193
Morelle-Olivia <hotoumorelle@gmail.com>
2022-02-10 09:35:08 +0100

    translation of amount-collected-pie

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -13,6 +13,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { AmountCollectedPieComponent } from './dashboard/amount-collected-pie/amount-collected-pie.component';
 import { AmountDisbursedPieComponent } from './dashboard/amount-disbursed-pie/amount-disbursed-pie.component';
 import { ClientTrendsBarComponent } from './dashboard/client-trends-bar/client-trends-bar.component';
+import {TranslateModule} from "@ngx-translate/core";
 
 /**
  * Home Component
@@ -20,11 +21,12 @@ import { ClientTrendsBarComponent } from './dashboard/client-trends-bar/client-t
  * Home and dashboard components should be declared here.
  */
 @NgModule({
-  imports: [
-    SharedModule,
-    PipesModule,
-    HomeRoutingModule
-  ],
+    imports: [
+        SharedModule,
+        PipesModule,
+        HomeRoutingModule,
+        TranslateModule
+    ],
   declarations: [
     HomeComponent,
     DashboardComponent,
